e42718b401a3846c619f83eeefb7e23996a2530b
Mayank Rajput (HackElite) <78653024+hackelite01@users.noreply.github.com>
2022-05-10 21:43:51 +0530

    helplist

---
+++ helplist/help.py
@@ -0,0 +1,153 @@
+import os
+from colorama import Fore
+import time
+import sys
+def Banner():
+
+    os.system("clear")
+    
+    print(Fore.LIGHTRED_EX+"""\n    
+  _  __      _       _     _   ____            
+ | |/ /_ __ (_) __ _| |__ | |_/ ___| _ __  _   _ 
+ | ' /| '_ \| |/ _' | '_ \| __\___ \| '_ \| | | |
+ | . \| | | | | (_| | | | | |_ ___) | |_) | |_| |
+ |_|\_\_| |_|_|\__, |_| |_|\__|____/| .__/ \__, |
+               |___/                |_|    |___/ 
+ =====================================================
+ **           Telegram : hackelite01                **
+ **           Channel : @hackelite01                **
+ **           Developers : Mayank Rajput            **
+ =====================================================          
+          """)       
+# ₿
+def infolist1():
+    time.sleep(0.1)
+    print(Fore.RED+" ["+Fore.WHITE+"$"+Fore.RED+"]"+Fore.CYAN+" Choose one of the options below \n")
+    time.sleep(0.1)
+    print(Fore.LIGHTYELLOW_EX+" [1] Information Gathering\n")
+    time.sleep(0.1)
+    print(Fore.RED+" [2] CMS Detection\n") 
+    time.sleep(0.1)
+    print(Fore.YELLOW+" [3] Developer :)\n")
+    time.sleep(0.1)
+    print(Fore.BLUE+" [4] Exit . . .\n")
+
+
+
+
+
+
+def infolist2():
+    time.sleep(0.1)
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+" - Bypass Cloud Flare")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.2)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Cms Detect")   
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Trace Toute")   
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [4]"+Fore.BLUE+" - Reverse IP")   
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [5]"+Fore.BLUE+" - Port Scan")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [6]"+Fore.BLUE+" - IP location Finder")
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [7]"+Fore.BLUE+" - Show HTTP Header")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [8]"+Fore.BLUE+" - Find Shared DNS")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [9]"+Fore.BLUE+" - Whois")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [10]"+Fore.BLUE+" - DNS Lookup")   
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [11]"+Fore.BLUE+"- Robots Scanner")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [12]"+Fore.BLUE+" - Admin Page Finder")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [13]"+Fore.BLUE+" - Back To Menu")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [14]"+Fore.BLUE+" - Exit :) \n")   
+
+def infolist3():
+
+      
+      Banner()
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.BLUE+"  Devloper : Mayank Rajput :) \n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.MAGENTA+" Github Profile  : hackelite01 :)\n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.CYAN+"  Our Telegram ID @hackelite01 :]\n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.CYAN+"  Our Instagram ID @hackelite01 :]\n")
+      time.sleep(0.1)
+      try:
+
+            input(Fore.LIGHTRED_EX+" [*]  Back To Menu (Press Enter...) ")
+      except:
+            print("")
+            print("\n")
+            sys.exit()
+
+def infolist4():
+
+    Banner()
+    
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+"- WordPress ")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Drupal"+Fore.RED+" Coming Soon . . .")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Joomla "+Fore.RED+" Coming Soon . . . ")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [4]"+Fore.BLUE+" - Back To Menu")   
+    
+    print(Fore.CYAN+"  **********************\n")  
+    time.sleep(0.1)
+
+
+def infowp():
+    Banner()
+    
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+" - Get Plugins ")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Get Username ")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Back To Menu ")
+
+    print(Fore.CYAN+"  **********************\n")
+    time.sleep(0.1)
